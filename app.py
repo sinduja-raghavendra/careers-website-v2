f95d30714aa90dd1801e86254aa2a42963f71ffe
sinduja-raghavendra <sindujak29@gmail.com>
2025-01-31 00:13:52 +0000

    Added HTML and CSS

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('static', 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 @app.route('/')
 def index():
-  return "Hello, World"
+  return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
